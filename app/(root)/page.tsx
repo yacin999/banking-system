026077e4e9e6ee2183f8e7ec81f1ef0d8de86ef8
yacine <yacinomar999@gmail.com>
2024-08-08 09:48:18 +0100

    create utils functions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 type Props = {}
@@ -15,6 +16,11 @@ const Home = (props: Props) => {
             title="Welcome"
             user={loggedIn?.firstName || "Guest"}
             subtext='Access and manage your account and transactions efficiently.'
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.5}
           />
         </header>
       </div>
