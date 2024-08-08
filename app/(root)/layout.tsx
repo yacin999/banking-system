c9bc60667740a96f1d963271c4803f41b429ac29
yacine <yacinomar999@gmail.com>
2024-08-08 19:46:41 +0100

    create mobile navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 
 export default function RootLayout({
@@ -12,6 +14,19 @@ export default function RootLayout({
   return (
     <main className="flex h-screen w-full font-inter">
         <Sidebar user={loggedIn}/>
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image
+              src={"/icons/logo.svg"}
+              width={30}
+              height={30}
+              alt="menu icon"
+            />
+            <div className="">
+              <MobileNav/>
+            </div>
+          </div>
+        </div>
         {children}
     </main>
   );
---
+++ components/MobileNav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const MobileNav = ({user}: MobileNavProps) => {
+  return (
+    <div>MobileNav</div>
+  )
+}
+
+export default MobileNav
