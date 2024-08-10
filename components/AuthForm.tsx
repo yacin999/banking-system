a1743fe7b6517d3dbfefd2b178cf8609a4eb43a2
yacine <yacinomar999@gmail.com>
2024-08-10 11:53:46 +0100

    create custominput

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,9 +17,13 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import CustomInput from './CustomInput'
  
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  email : z.string().email(),
+  password : z.string().min(8, {
+    message : "Password must be at least 8 chars"
+  })
 })
 
 type Props = {
@@ -33,7 +37,8 @@ const AuthForm = ({type}: Props) => {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-          username: "",
+          email : "",
+          password : ""
         },
     })
      
@@ -73,21 +78,20 @@ const AuthForm = ({type}: Props) => {
             <>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                        <FormField
-                        control={form.control}
-                        name="username"
-                        render={({ field }) => (
-                            <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage />
-                            </FormItem>
-                        )}
+                        <CustomInput
+                            form={form}
+                            type='email'
+                            name='email'
+                            label='Email'
+                            placeholder='Enter your email'
+                        />
+
+                        <CustomInput
+                            form={form}
+                            type='password'
+                            name='password'
+                            label='Password'
+                            placeholder='Enter your password'
                         />
                         <Button type="submit">Submit</Button>
                     </form>
