4fa425747fba8bc3ece1aa364b0095d7c97a9f44
yacin999 <yacinomar999@gmail.com>
2024-08-18 19:38:17 +0100

    create getbanks

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,7 +2,7 @@
 
 import { cookies } from "next/headers"
 import { createAdminClient, createSessionClient } from "../appwrite"
-import { ID } from "node-appwrite"
+import { ID, Query } from "node-appwrite"
 import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils"
 import { redirect } from "next/navigation"
 import { plaidClient } from "../plaid"
@@ -232,4 +232,40 @@ export const exchangePublicToken = async({
     } catch (error) {
       console.error("An error occurred while creating exchanging token:", error);
     }
+}
+
+
+export const getBanks = async ({userId} : getBanksProps) => {
+  try {
+    const { database } = await createAdminClient()
+
+    const banks = await database.listDocuments(
+      DATABASE_ID!,
+      BANK_COLLECTION_ID!,
+      [Query.equal('userId', [userId])]
+    )
+
+
+    return parseStringify(banks.documents)
+  } catch (error) {
+    console.log("error from GETBANKS :", error)
+  }
+}
+
+
+export const getBank = async ({documentId} : getBankProps) => {
+  try {
+    const { database } = await createAdminClient()
+
+    const bank = await database.listDocuments(
+      DATABASE_ID!,
+      BANK_COLLECTION_ID!,
+      [Query.equal('$id', [documentId])]
+    )
+
+
+    return parseStringify(bank.documents[0])
+  } catch (error) {
+    console.log("error from GETBANK :", error)
+  }
 }
