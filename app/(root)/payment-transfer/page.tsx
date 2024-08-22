4a3496d5b96bbc09158fabaad6132809ce4e7834
yacin999 <yacinomar999@gmail.com>
2024-08-22 18:12:43 +0100

    create payment transfer

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,12 +1,20 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 type Props = {}
 
 const paymentTransfer = (props: Props) => {
   return (
-    <div className='flex flex-col'>
-      <div>flex</div>
-    </div>
+    <section className='payment-transfer'>
+      <HeaderBox
+        title='Payment Transfer'
+        subtext='Please provide any specific details or notes related to the payment transfers'
+      />
+
+      <section className='size-full pt-5'>
+        <PaymentTransferForm/>
+      </section>
+    </section>
   )
 }
 
