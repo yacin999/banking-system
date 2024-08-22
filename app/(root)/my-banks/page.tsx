ac9572f6abfc3c34bfa715042535e00b541648e7
yacin999 <yacinomar999@gmail.com>
2024-08-22 13:01:35 +0100

    rectify getaccounts

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,10 +1,36 @@
+import BankCard from '@/components/BankCard'
+import HeaderBox from '@/components/HeaderBox'
+import { getAccounts } from '@/lib/actions/bank.actions'
+import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
 
 type Props = {}
 
-const MyBanks = (props: Props) => {
+const MyBanks = async (props: Props) => {
+  const loggedIn = await getLoggedInUser()
+  const accounts = await getAccounts({userId : loggedIn.$id})
   return (
-    <div className='text-3xl'>page</div>
+    <section className='flex'>
+      <div className='my-banks'>
+        <HeaderBox
+          title='My Bank Accounts'  
+          subtext='Effortlessly manage your banking activities.'
+        />
+
+        <div className='space-y-4'>
+          <h2 className='header-2'>Your cards</h2>
+          <div className='flex flex-wrap gap-6'>
+            {accounts && accounts.data.map((a : Account)=> (
+              <BankCard
+                key={accounts.id}
+                account={a}
+                userName={loggedIn?.firstName}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
