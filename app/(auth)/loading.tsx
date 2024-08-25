82d13b5b0b0f2cc3e0b37b79593b8f30851192ba
yacin999 <yacinomar999@gmail.com>
2024-08-25 11:03:44 +0100

    add loading animation

---
+++ app/(auth)/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const loading = (props: Props) => {
+  return (
+    <div className='flex items-center justify-center font-bold'>
+        Loading...
+    </div>
+  )
+}
+
+export default loading
