6f82ef7c24c2c4b29224a52118b89b189f96202c
yacin999 <yacinomar999@gmail.com>
2024-09-06 20:46:07 +0100

    update sign up page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 
 const SignUp = async () => {
-
+  // sign up
   return (
     <section className='flex-center size-full max-sm:px-6'>
       <AuthForm type={"sign-up"}/>
