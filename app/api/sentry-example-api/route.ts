374ce555bc8bd88f78180450c2d9e4759590b6fb
yacin999 <yacinomar999@gmail.com>
2024-09-14 20:21:48 +0100

    update sentry route

--- app/api/sentry-example-api/route.ts
+++ app/api/sentry-example-api/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 
-// sentry route
 // A faulty API route to test Sentry's error monitoring
 export function GET() {
   throw new Error("Sentry Example API Route Error");
