3d165c8a6814926357869d0fe39a42c64ef9c632
yacin999 <yacinomar999@gmail.com>
2024-09-14 20:23:17 +0100

    update signup paeg

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -2,7 +2,7 @@ import AuthForm from '@/components/AuthForm'
 import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
 
-
+// sign up page
 const SignUp = async () => {
   return (
     <section className='flex-center size-full max-sm:px-6'>
