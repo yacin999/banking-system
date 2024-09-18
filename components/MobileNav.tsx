304ae5df6b61f269d36ec12f06472897b13cc043
yacin999 <yacinomar999@gmail.com>
2024-09-18 20:22:14 +0100

    update mobile nav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -16,6 +16,8 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 import Footer from "./Footer"
 
+
+// mobile nav
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
 
